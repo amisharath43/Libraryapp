7ddfe45b6f44c63a41aa060f10d12008a8af70ae
Amisha Rath <amisharath02@gmail.com>
2026-03-30 22:15:33 +0530

    change in program.csfile

--- Libraryapp/Program.cs
+++ Libraryapp/Program.cs
@@ -9,21 +9,27 @@ namespace Libraryapp
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add services to the container. 
             builder.Services.AddControllersWithViews();
             builder.Services.AddEndpointsApiExplorer();
-
+            builder.Services.AddSwaggerGen();
 
             builder.Services.AddDbContext<LibraryContext>(options =>
-                   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Configure the HTTP request pipeline. 
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts. 
                 app.UseHsts();
             }
 
@@ -33,17 +39,14 @@ namespace Libraryapp
             app.UseAuthorization();
 
             app.MapStaticAssets();
-            //app.MapControllerRoute(
-            // name: "default",
-            // pattern: "{controller=Home}/{action=Index}/{id?}")
-            //.WithStaticAssets();
+            //app.MapControllerRoute( 
+            // name: "default", 
+            // pattern: "{controller=Home}/{action=Index}/{id?}") 
+            //.WithStaticAssets(); 
 
             app.MapControllers();
 
-
-
             app.Run();
-
 
         }
     }
